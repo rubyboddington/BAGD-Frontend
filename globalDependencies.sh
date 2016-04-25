2fe61a63af62e7c5dd4985a110e1b6aebb9e8e13
limzykenneth <limzy.kenneth@gmail.com>
2016-04-25 12:41:58 +0100

    Improvement and examples on easy mode

--- globalDependencies.sh
+++ globalDependencies.sh
@@ -3,7 +3,7 @@
 echo "This script will install the following npm package into your global package folder:"
 echo ""
 echo "postcss-simple-vars"
-echo "postcss-nesting"
+echo "postcss-nested"
 echo "autoprefixer"
 
 TMOUT=10
@@ -16,7 +16,7 @@ if [[ -z $REPLY ]]; then
 fi
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	npm install -g postcss-simple-vars postcss-nesting autoprefixer
+	npm install -g postcss-simple-vars postcss-nested autoprefixer
 else
 	echo "Global dependencies not installed, don't worry if you already have them."
 fi
