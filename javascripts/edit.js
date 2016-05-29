9b3b6c42683e08e2fbec412747a7e2e9cc4e2e50
limzykenneth <limzy.kenneth@gmail.com>
2016-05-29 11:06:39 +0100

    Add show details

--- javascripts/edit.js
+++ javascripts/edit.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	
+	holdingPageDetails();
+	$(window).resize(holdingPageDetails);
 });
+
+function holdingPageDetails(){
+	if(window.innerWidth >= 480){
+		$("header.holding-page u, header.holding-page #show-details").hover(function() {
+			$("header.holding-page #show-details").css('max-height', '1500px');
+		}, function() {
+			$("header.holding-page #show-details").css('max-height', '0px');
+		});
+	}
+}
