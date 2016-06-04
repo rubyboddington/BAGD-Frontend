65a1b428dab4a2e6f9700e09839f684483f5cbba
limzykenneth <limzy.kenneth@gmail.com>
2016-06-04 20:12:38 +0100

    Add some hover and active state

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,7 +2,22 @@ $(document).ready(function() {
 	holdingPageDetails();
 	$(window).resize(holdingPageDetails);
 
+	$("#page-content #tags-nav li a").click(function(e){
+		$(this).toggleClass('active');
 
+		var txt = $(this).text();
+		if($(this).hasClass("active")){
+			txt = "+" + txt.substring(1);
+		}else{
+			txt = "-" + txt.substring(1);
+		}
+		$(this).text(txt);
+	});
+
+	$("#page-content #names-nav li a").click(function(e){
+		$("#page-content #names-nav li a").removeClass("active");
+		$(this).toggleClass("active");
+	});
 
 });
 
