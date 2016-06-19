302e6decce2c53680411b7e82c11aaa002b10aa1
limzykenneth <limzy.kenneth@gmail.com>
2016-06-19 16:35:30 +0100

    Remove name list alphabet header

--- javascripts/collectionView.js
+++ javascripts/collectionView.js
@@ -32,7 +32,7 @@ module.exports = Backbone.View.extend({
 		}, this);
 
 		_.each(groupedList, function(el, key){
-			var header = "<ul><li><h6>" + key + "</h6></li>";
+			var header = "<ul>";
 
 			wholeList += header;
 
